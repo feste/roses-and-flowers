1018a3c1dcae268c0f4d7041f8135e69d6ae497e
erin <festethejester@gmail.com>
2014-06-03 17:14:38 -0700

    saving mturk interface files

---
+++ mturk3/forced-roses-postHIT.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+cd /home/feste/opt/aws-mturk-clt-1.3.1/bin
+./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /home/feste/CoCoLab/roses-and-flowers/mturk3/forced-roses -input /home/feste/CoCoLab/roses-and-flowers/mturk3/forced-roses.input -question /home/feste/CoCoLab/roses-and-flowers/mturk3/forced-roses.question -properties /home/feste/CoCoLab/roses-and-flowers/mturk3/forced-roses.properties -maxhits 1
